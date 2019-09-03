0091e971315ebaed3040dd0b370a0bc7326eac50
myvary <727484328@qq.com>
2019-09-03 16:09:53 +0800

    inti

---
+++ mangosteen-server/src/main/java/com/orchardsoil/mangosteenserver/MangosteenServerApplication.java
@@ -0,0 +1,13 @@
+package com.orchardsoil.mangosteenserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MangosteenServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MangosteenServerApplication.class, args);
+    }
+
+}
