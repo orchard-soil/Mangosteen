080a2868192415f1be769b9a5cc8d2a666a9320b
myvary <727484328@qq.com>
2019-09-09 11:47:34 +0800

    add druid

--- mangosteen-server/src/main/java/com/orchardsoil/mangosteenserver/MangosteenServerApplication.java
+++ mangosteen-server/src/main/java/com/orchardsoil/mangosteenserver/MangosteenServerApplication.java
@@ -1,5 +1,6 @@
 package com.orchardsoil.mangosteenserver;
 
+import com.alibaba.druid.spring.boot.autoconfigure.DruidDataSourceAutoConfigure;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -8,11 +9,13 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-@SpringBootApplication
+
 @EnableTransactionManagement
 @EnableScheduling
 @EnableAsync
 @EnableSwagger2
+//排除 原生Druid的快速配置类
+@SpringBootApplication(exclude = DruidDataSourceAutoConfigure.class)
 @MapperScan(" com.orchardsoil.mangosteenserver.core.mapper")
 public class MangosteenServerApplication {
 
