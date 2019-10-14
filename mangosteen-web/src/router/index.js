5458ce21abda622dff05361d3339f46768fec35e
ws <942590550@qq.com>
2019-10-14 16:46:19 +0800

    配置默认页路由

--- mangosteen-web/src/router/index.js
+++ mangosteen-web/src/router/index.js
@@ -11,13 +11,17 @@ Vue.use(VueRouter);
 export default new VueRouter({
     // 3.1 配置一级路由
     routes: [
-        {
+        { // 登录页
             path: '/login',
             component: Login,
         },
-        {
+        { // 主页
             path: '/admin',
             component: Admin
+        },
+        {// 默认到登录页
+            path: '/',
+            component: Login
         }
     ]
 })
